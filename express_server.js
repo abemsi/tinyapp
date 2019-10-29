66ac6daab6162120e39992a093d195a50c024e85
Amy Bemister <amy.bemister@gmail.com>
2019-10-29 15:28:52 +0000

    Added redirection to the original longURL from the short URL

--- express_server.js
+++ express_server.js
@@ -43,6 +43,11 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 app.post('/urls', (req, res) => {
   let longURL = req.body.longURL;
   let randomString = generateRandomString();
