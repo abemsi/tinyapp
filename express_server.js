8e2bc6da3ed431248bda2c6776ec5bfedf08dbfa
Amy Bemister <amy.bemister@gmail.com>
2019-10-30 01:22:55 +0000

    Passed in the username to all views that include the header partial to display username

--- express_server.js
+++ express_server.js
@@ -20,12 +20,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-let templateVars = {
-  username: req.cookies["username"],
-  // ... any other vars
-};
-res.render("urls_index", templateVars);
-
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
@@ -39,16 +33,22 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls:urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = {
+    username: req.cookies["username"]
+   };
+  res.render('urls_new', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
