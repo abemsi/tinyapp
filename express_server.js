dd59254b09a730bae8ae25e5ae3f33c442f75b9f
Amy Bemister <amy.bemister@gmail.com>
2019-10-30 01:50:25 +0000

    Added logout route that clears the username cookie

--- express_server.js
+++ express_server.js
@@ -81,6 +81,12 @@ app.post('/login', (req, res) => {
   res.redirect(`/urls`);
 })
 
+app.post('/logout', (req, res) => {
+  let username = req.body.username;
+  res.clearCookie('username', username);
+  res.redirect(`/urls`);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
